1d7e2f438875ed120154982fde8ce1ca033fd176
Philipp SL Schäfer <philipp.schaefer98@icloud.com>
2026-02-17 09:17:27 +0100

    updating documentation

--- docs/conf.py
+++ docs/conf.py
@@ -95,7 +95,13 @@ intersphinx_mapping = {
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", "**.ipynb_checkpoints"]
+exclude_patterns = [
+    "_build",
+    "Thumbs.db",
+    ".DS_Store",
+    "**.ipynb_checkpoints",
+    "notebooks/cross_condition_lupus.ipynb",  # for now we do not want to include this in the doucmentation
+]
 
 
 # -- Options for HTML output -------------------------------------------------
